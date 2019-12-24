da5ad212e5d7e8443e6796e64f3aac4a9a635b99
Clement Calmels <clement.calmels@free.fr>
2019-12-24 12:00:36 +0100

    split host:/dir part earlier ?

--- backup.sh
+++ backup.sh
@@ -10,21 +10,20 @@ usage: $basename rsync <source_dir> <[host:]backup_dir> [<number_of_increment>]
 
 backup_rsync() {
     local src="$1"
-    local remote="${2%%:*}:"
+    local dest="$2"
     local dir="${2##*:}"
     local exclude_file="$src/.backup-exclude"
     local exclude_options
 
     # remove any trailing '/'
     dir="${dir%%/}"
-    [ "$remote" = "$2:" ] && remote=""
 
     [ -r "$exclude_file" ] &&
 	exclude_options="--delete-excluded --exclude-from "$exclude_file""
 
     echo "Rsyncing"
     rsync -ax -H --delete $exclude_options \
-	  --link-dest="$dir.1/" "$src/" "$remote$dir/"
+	  --link-dest="$dir.1/" "$src/" "$dest/"
 }
 
 mday() {
